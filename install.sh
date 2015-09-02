81236f04d023ecabc802a9d713edafcffdb4d7a5
Teddy Widom <theodore.widom@gmail.com>
2015-09-02 10:13:07 -0400

    Add tmux config

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ git pull --recurse-submodules
 git submodule update --recursive
 
 dir=`pwd`
-files_to_link="bashrc gemrc gitconfig gitignore rspec vim vimrc zshenv zshrc"
+files_to_link="bashrc gemrc gitconfig gitignore rspec tmux.conf vim vimrc zshenv zshrc"
 
 for file in $files_to_link; do
   echo "Creating symlink from $dir/$file to ~/.$file in home directory."
